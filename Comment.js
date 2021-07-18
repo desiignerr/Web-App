924d6ae94cdb5f66ba6557f464c379a9e002be0a
blessingphilemont <83703725+blessingphilemont@users.noreply.github.com>
2021-07-18 20:36:22 +0100

    Comment file has been develop for every user.

---
+++ Comment.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+
+const CommentSchema = new mongoose.Schema(
+
+
+
+
+);
+
+
+const Comment = mongoose.model('User', CommentSchema);
+
+module.exports = Comment;
